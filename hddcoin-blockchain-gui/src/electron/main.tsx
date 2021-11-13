11fbb03747c979d8b3dd486b0119077cb00b367c
HDDcoin Network <code@hddcoin.org>
2021-11-12 21:59:36 -0600

    Added Chia Upstream 1.2.11 changes
    Added Chia Upstream 1.2.11 changes

--- hddcoin-blockchain-gui/src/electron/main.tsx
+++ hddcoin-blockchain-gui/src/electron/main.tsx
@@ -395,7 +395,7 @@ if (!handleSquirrelEvent()) {
               );
             },
           },
-			//{
+          //{
            // type: 'separator',
           //},
           {
@@ -474,92 +474,6 @@ if (!handleSquirrelEvent()) {
         ],
       },
     ];
-		
-	if (process.platform === 'darwin') {
-      // Chia Blockchain menu (Mac)
-      template.unshift({
-        label: i18n._(/* i18n */ { id: 'Chia' }),
-        submenu: [
-          {
-            label: i18n._(/* i18n */ { id: 'About Chia Blockchain' }),
-            click: () => {
-              openAbout();
-            },
-          },
-          {
-            type: 'separator',
-          },
-          {
-            role: 'services',
-          },
-          {
-            type: 'separator',
-          },
-          {
-            role: 'hide',
-          },
-          {
-            role: 'hideothers',
-          },
-          {
-            role: 'unhide',
-          },
-          {
-            type: 'separator',
-          },
-          {
-            role: 'quit',
-          },
-        ],
-      });
-	
-      // File menu (MacOS)
-      template.splice(1, 1, {
-        label: i18n._(/* i18n */ { id: 'File' }),
-        submenu: [
-          {
-            role: 'close',
-          },
-        ],
-      });
-
-      // Edit menu (MacOS)
-      template[2].submenu.push(
-        {
-          type: 'separator',
-        },
-        {
-          label: i18n._(/* i18n */ { id: 'Speech' }),
-          submenu: [
-            {
-              role: 'startspeaking',
-            },
-            {
-              role: 'stopspeaking',
-            },
-          ],
-        },
-      );
-
-      // Window menu (MacOS)
-      template.splice(4, 1, {
-        role: 'window',
-        submenu: [
-          {
-            role: 'minimize',
-          },
-          {
-            role: 'zoom',
-          },
-          {
-            type: 'separator',
-          },
-          {
-            role: 'front',
-          },
-        ],
-      });
-    }
 
     if (process.platform === 'linux' || process.platform === 'win32') {
       // Help menu (Windows, Linux)
