7acbb45753974e6763f333698824a30fe98947be
zcomputerwiz <zcomputerwiz@users.noreply.github.com>
2022-01-15 12:28:20 -0600

    try original wallet ts for sanity check

--- hddcoin-blockchain-gui/src/components/hddapps/wallet/HDDappsHODLWallet.tsx
+++ hddcoin-blockchain-gui/src/components/hddapps/wallet/HDDappsHODLWallet.tsx
@@ -4,14 +4,25 @@ import {
   Box,
   Typography,
   Tabs,
+  Tab,
+  Container,
 } from '@material-ui/core';
 import styled from 'styled-components';
-import { useSelector } from 'react-redux';
+// import { useRouteMatch, useHistory } from 'react-router';
+import { /*useDispatch, */ useSelector } from 'react-redux';
 import { FormatLargeNumber } from '@hddcoin/core';
-import StandardHODLWallet from './standard/WalletStandard';
+import StandardWallet from '../../wallet/standard/WalletStandard';
+import { CreateWalletView } from '../../wallet/create/WalletCreate';
+import ColouredWallet from '../../wallet/coloured/WalletColoured';
+import RateLimitedWallet from '../../wallet/rateLimited/WalletRateLimited';
+import DistributedWallet from '../../wallet/did/WalletDID';
 import type { RootState } from '../../../modules/rootReducer';
+import WalletType from '../../../constants/WalletType';
 import LayoutMain from '../../layout/LayoutMain';
-import { useHistory, useRouteMatch, useParams } from 'react-router-dom';
+import config from '../../../config/config';
+import { Switch, Route, useHistory, useRouteMatch, useParams } from 'react-router-dom';
+
+const { multipleWallets } = config;
 
 const StyledTabs = styled(Tabs)`
   flex-grow: 1;
@@ -72,6 +83,16 @@ export function StatusCard() {
   );
 }
 
+function TabPanel(props) {
+  const { children, value, selected } = props;
+
+  if (value === selected) {
+    return children;
+  }
+
+  return null;
+}
+
 export default function HODLWallet() {
   const history = useHistory();
   const { walletId } = useParams();
@@ -79,13 +100,68 @@ export default function HODLWallet() {
   const wallets = useSelector((state: RootState) => state.wallet_state.wallets);
   const loading = !wallets;
 
+  function handleChange(_, newValue) {
+    history.push(`/dashboard/wallets/${newValue}`);
+  }
+
+  // redirect to default "standard wallet" when no wallet was selected
+  useEffect(() => {
+    if (!walletId && wallets) {
+      history.push('/dashboard/wallets/1');
+    }
+  }, [wallets, walletId]);
+
   return (
     <LayoutMain
       loading={loading}
       loadingTitle={<Trans>Loading list of wallets</Trans>}
       title={<Trans>Wallets</Trans>}
+      bodyHeader={multipleWallets ? (
+        <Container maxWidth="lg">
+          <StyledTabs
+            value={walletId || '1'}
+            onChange={handleChange}
+            indicatorColor="primary"
+            textColor="primary"
+            scrollButtons="auto"
+            variant="scrollable"
+          >
+            {wallets?.map((wallet) => (
+              <Tab label={wallet.name} value={String(wallet.id)} key={wallet.id} />
+            ))}
+            <Tab value="create" label={<Trans>+ Add Wallet</Trans>} />
+          </StyledTabs>
+        </Container>
+      ) : undefined}
     >
-      <StandardHODLWallet wallet_id={1} showTitle />
+      {multipleWallets ? (
+        <Switch>
+          {wallets?.map((wallet) => (
+            <Route path={`${path}/${wallet.id}`} key={wallet.id}>
+              {wallet.type === WalletType.STANDARD_WALLET && (
+                <StandardWallet wallet_id={wallet.id} />
+              )}
+
+              {wallet.type === WalletType.COLOURED_COIN && (
+                <ColouredWallet wallet_id={wallet.id} />
+              )}
+
+              {wallet.type === WalletType.RATE_LIMITED && (
+                <RateLimitedWallet wallet_id={wallet.id} />
+              )}
+
+              {wallet.type === WalletType.DISTRIBUTED_ID && (
+                <DistributedWallet walletId={wallet.id} />
+              )}
+            </Route>
+          ))}
+          <Route path={`/dashboard/wallets/create`}>
+            <CreateWalletView />
+          </Route>
+        </Switch>
+      ) : (
+        <StandardWallet wallet_id={1} showTitle />
+      )}
     </LayoutMain>
   );
 }
